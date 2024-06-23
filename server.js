288ba4f0a2dfd58127300d72af72a4a130a69a90
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-23 13:14:54 -0300

    Arquivos iniciais

---
+++ server.js
@@ -0,0 +1,109 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { MercadoPagoConfig, Preference } from "mercadopago";
+
+dotenv.config();
+
+const app = express();
+const connect = process.env.PORT || "3001";
+
+app.use(cors({
+  origin: '*', // Permite apenas requisições desta origem
+  methods: ['GET', 'POST'], // Permite apenas esses métodos
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Idempotency-Key'], // Permite apenas esses cabeçalhos
+}));
+
+app.use(express.json());
+
+// https://developers.mercadopago.com/panel
+const token = process.env.TOKEN_ACCESS
+//const integrator = process.env.SECRET_INTEGRATOR
+const mercadoPagoClient = new MercadoPagoConfig({
+  accessToken: token,
+  //integrator_id: integrator
+})
+
+app.get("/order", function (_, res) {
+  res.send("Servidor está funcionando");
+});
+
+app.post("/order/create_preference", async function (req, res) {
+  try {
+    const externalReference = req.body.external_reference;
+    const idempotencyKey = req.headers["X-Idempotency-Key"];
+    console.log("Received request body:", req.body);
+
+    if (!req.body.items || req.body.items.length === 0) {
+      throw new Error("Dados incompletos para criar a preferência.");
+    }
+
+    const items = req.body.items.map(item => ({
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      picture_url: item.picture_url,
+      category_id: item.category_id,
+      quantity: Number(item.quantity),
+      unit_price: Number(item.unit_price),
+      currency_id: "BRL",
+    }));
+
+    const body = {
+      external_reference: externalReference,
+      items,
+      back_urls: {
+        success: "https://tuaneeduan.com.br/ecommerce",
+        failure: "https://tuaneeduan.com.br/ecommerce",
+        pending: "https://tuaneeduan.com.br/ecommerce",
+      },
+      auto_return: "approved",
+      notification_url: "https://tuaneeduan.com.br/ecommerce/webhook",
+      payment_methods: {
+        installments: 12
+      },
+    };
+
+    console.log("Formatted body:", body);
+
+    const preference = new Preference(mercadoPagoClient);
+    const result = await preference.create({ body, idempotencyKey });
+
+    res.json({
+      id: result.body.id,
+    });
+
+  } catch (error) {
+    console.log("Erro ao criar preferência de pagamento:", error);
+    res.status(500).json({
+      error: "Erro ao criar preferência de pagamento",
+      message: error.message,
+    });
+  }
+});
+
+app.post("/webhook", async function (req, res) {
+  const paymentId = req.query.id;
+  try {
+    const response = await fetch(`https://api.mercadopago.com/v1payments/${paymentId}`, {
+      method: 'GET',
+      headers: {
+        "Authorization": `Bearer ${mercadoPagoClient.accessToken}`
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error", error);
+    res.sendStatus(500);
+  }
+})
+
+app.listen(connect, () => {
+  console.log(`Servidor executando na porta ${connect}`)
+})
+
